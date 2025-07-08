bc2805e18bc146cb2224f9c43d2566f6d72c52f5
Kanav Dahat <kanavdahat55@gmail.com>
2025-07-08 23:39:37 +0530

    Added some more problems for practise regarding Time complexity arrays optimized

--- JavaArrays/src/JavaArrays/TripleSumOptimized.java
+++ JavaArrays/src/JavaArrays/TripleSumOptimized.java
@@ -1,40 +1,38 @@
 //Problem statement
-//You have been given an integer array/list(ARR) and a number 'num'. Find and return the total number of pairs in the array/list which sum to 'num'.
+//You have been given a random integer array/list(ARR) and a number X. Find and return the triplet(s) in the array/list which sum to X.
 //
-//Note:
-//Given array/list can contain duplicate elements. 
+//Note :
+//Given array/list can contain duplicate elements.
 //Detailed explanation ( Input/output format, Notes, Images )
 //Constraints :
 //1 <= t <= 10^2
-//0 <= N <= 10^4
-//0 <= num <= 10^9
+//0 <= N <= 10^3
+//0 <= X <= 10^9
 //
 //Time Limit: 1 sec
 //Sample Input 1:
 //1
-//9
-//1 3 6 2 5 4 3 2 4
 //7
+//1 2 3 4 5 6 7 
+//12
 //Sample Output 1:
-//7
+//5
 //Sample Input 2:
 //2
+//7
+//1 2 3 4 5 6 7 
+//19
 //9
-//1 3 6 2 5 4 3 2 4
-//12
-//6
-//2 8 10 5 -2 5
+//2 -5 8 -6 0 5 10 11 -3
 //10
 //Sample Output 2:
 //0
-//2
+//5
 //
 //
 // Explanation for Input 2:
-//Since there doesn't exist any pair with sum equal to 12 for the first query, we print 0.
-//
-//For the second query, we have 2 pairs in total that sum up to 10. They are, (2, 8) and (5, 5).
-
+//Since there doesn't exist any triplet with sum equal to 19 for the first query, we print 0.
+//For the second query, we have 5 triplets in total that sum up to 10. They are, (2, 8, 0), (2, 11, -3), (-5, 5, 10), (8, 5, -3) and (-6, 5, 11)
 
 
 
@@ -42,9 +40,7 @@ package JavaArrays;
 
 import java.util.Scanner;
 
-public class PairSumOptimized {
-	
-	
+public class TripleSumOptimized {
 	
 	public static void InsertionSort(int[] arr) {
 		if(arr.length==1) {
@@ -84,10 +80,23 @@ public class PairSumOptimized {
 		return count;
 	}
 	
+	public static int tripletSum(int[] arr, int n) {
+		InsertionSort(arr);
+		int count =0;
+		for(int i =0;i<arr.length;i++) {
+			int num=n-arr[i];
+			if(i+1<arr.length && i+1!=arr.length) {
+				int smallCount=pairSum(arr,num,i+1,arr.length-1);
+				count=count+smallCount;
+			}
+			
+		}
+		return count;
+	}
+	
 	public static int pairSum(int[] arr, int num,int si,int ei) {
 		//Your code goes here
 		int sumPair = 0;
-		InsertionSort(arr);
 		int i=si;
 		int j =ei;
 		while(i<j) {
@@ -127,6 +136,5 @@ public class PairSumOptimized {
 		Scanner s = new Scanner(System.in);
 		int[] arr = TakeArrayInput();
 		int n = s.nextInt();
-		System.out.println(pairSum(arr,n,0,arr.length-1));
 	}
 }
